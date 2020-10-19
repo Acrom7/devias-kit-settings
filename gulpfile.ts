3be21b8451c9518784545696affe977eb5e0d2bc
Acrom <zvez.dmtr@gmail.com>
2020-10-19 11:09:16 +1000

    Add SASS support with UIkit

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,11 +6,15 @@ const source = require('vinyl-source-stream')
 const fancy_log = require('fancy-log')
 const pug = require('gulp-pug')
 const del = require('del')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
 
 const paths = {
 	pugEntry: './src/views/index.pug',
 	tsEntry: 'src/main.ts',
+	sassEntry: 'src/sass/styles.sass',
 	publicDir: './dist',
+	assets: 'src/assets',
 }
 
 const browserifyInstance = browserify({
@@ -26,10 +30,12 @@ function serve() {
 			baseDir: paths.publicDir,
 		},
 		open: false,
+		notify: false,
 	})
 
 	watch('src/*.ts').on('change', series(bundleJs, browserSync.reload))
 	watch('src/views/*.pug').on('change', series(buildPug, browserSync.reload))
+	watch('src/sass/**/*.sass').on('change', buildSass)
 }
 
 function buildPug() {
@@ -48,10 +54,23 @@ function bundleJs() {
 	.pipe(dest(paths.publicDir))
 }
 
+function buildSass() {
+	return src(paths.sassEntry)
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write())
+	.pipe(dest(paths.publicDir))
+	.pipe(browserSync.stream())
+}
+
+function copyAssets() {
+	return src(paths.assets + '/*').pipe(dest(paths.publicDir + '/assets'))
+}
+
 function clean() {
 	return del(`${paths.publicDir}/*`)
 }
 
-exports.build = series(clean, parallel(bundleJs, buildPug))
+exports.build = series(clean, parallel(bundleJs, buildPug, buildSass, copyAssets))
 exports.default = series(exports.build, serve)
 
