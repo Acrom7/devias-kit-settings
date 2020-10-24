c6b5b16cdb0525d76ac331e358dc4bf1a3204d64
Acrom <zvez.dmtr@gmail.com>
2020-10-24 14:20:24 +1000

    Add Hamburger menu component

---
+++ src/components/hamburger-button/hamburger-button.ts
@@ -0,0 +1,15 @@
+function initHamburger(offcanvasId: string): void {
+	const button: HTMLElement = document.getElementById('hamburger-button')
+	const offcanvas: HTMLElement = document.getElementById(offcanvasId)
+
+	button.setAttribute('data-uk-toggle', `target: #${offcanvasId}`)
+
+	button.addEventListener('click', () => {
+		button.classList.toggle('opened')
+	})
+	offcanvas.addEventListener('beforehide', () => {
+		button.classList.remove('opened')
+	})
+}
+
+module.exports = initHamburger
